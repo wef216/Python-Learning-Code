8f40aeba78abb9b056dd35606688a4a81d78099c
wef216 <wef216@gmail.com>
2020-03-31 23:19:07 -0400

    Initial Commitment

--- A Pythonic Card Deck.py
+++ A Pythonic Card Deck.py
@@ -6,8 +6,8 @@ class FrenchDeck:
     ranks = [str(n) for n in range(2,11)] + list('JQKA')
     suits = "spades diamonds clubs hearts".split()
 
-    def __init___(self):
-        self._cards = [Card(rank, suits) for suit in self.suits
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
                                          for rank in self.ranks]
                                         
     def __len__(self):
@@ -17,7 +17,21 @@ class FrenchDeck:
         return self._cards[position]
 
 deck = FrenchDeck()
+print(len(deck))
+print(deck[1])
+
+### Randomly choose a card
+from random import choice
+print(choice(deck))
+print(choice(deck))
 
 
-beer_card = Card('7', 'diamond')
-print(beer_card)
+### deck is iterable
+for card in deck[1:14]:
+    print(card)
+
+
+### in method
+print(Card("A","spades") in deck)
+print(Card("As","spades") in deck) 
+
